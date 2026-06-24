669d9df41a3d0cbc47a0f680d1ac0a8b8028c4af
Tran Xuan Son <110320455+Yamaaaaaaaa@users.noreply.github.com>
2026-06-24 23:32:10 +0700

    Fix warning FE

--- fe_next/app/layout.tsx
+++ fe_next/app/layout.tsx
@@ -27,8 +27,12 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col bg-slate-50 text-slate-800">
+      <body
+        className="min-h-full flex flex-col bg-slate-50 text-slate-800"
+        suppressHydrationWarning
+      >
         <SessionProvider>
           {children}
         </SessionProvider>
